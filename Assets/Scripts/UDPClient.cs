a8b1e3e669418b4aa15e3ff8dd64515a8c2b1f97
irenecho75 <irenecho75@gmail.com>
2021-07-01 04:04:08 -0700

    animators and prefabs

--- Assets/Scripts/UDPClient.cs
+++ Assets/Scripts/UDPClient.cs
@@ -5,7 +5,8 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
+using System.Linq;
+using System.Net.NetworkInformation;
 public class UDPClient : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,20 +14,34 @@ public class UDPClient : MonoBehaviour
     {
         UDPTest();
     }
-
+    
     void UDPTest() {
         UdpClient client = new UdpClient(5600);
+
         try {
             client.Connect("127.0.0.1", 5500); 
             // server used on my own computer, local host
 
-            byte[] sendBytes = Encoding.ASCII.GetBytes("Hello from the client");
-            // converts the string to a byte array
+           UdpSend(client);
+           UdpReceive(client);
+           client.Close();
+           // Closes the client
+        }
+        catch (Exception e) {
+            Debug.Log("Exception thrown: " + e.Message);
+        }
+    }
+    void UdpSend(UdpClient client) {
+        byte[] sendBytes = Encoding.ASCII.GetBytes("Hello from the client");
+        // converts the string to a byte array
 
-            client.Send(sendBytes, sendBytes.Length);
-            // client sends a message
-
+        client.Send(sendBytes, sendBytes.Length);
+        // client sends a message
+    }
+    void UdpReceive(UdpClient client) {
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            // Any indicates that Socket instance must listen for client activity 
+            // on all network interfaces.
 
             byte[] receiveBytes = client.Receive(ref remoteEndPoint); 
             // Receive blocks execution until it receives a message
@@ -35,10 +50,6 @@ public class UDPClient : MonoBehaviour
             string receivedString = Encoding.ASCII.GetString(receiveBytes);
 
             print("Message received from the server \n" + receivedString);
-        }
-        catch (Exception e){
-            print("Exception thrown " + e.Message);
-        }
     }
 
 }
