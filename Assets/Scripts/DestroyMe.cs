76e9588668a1a6b898d136c9df15d751c96b61c3
irenecho75 <irenecho75@gmail.com>
2021-07-06 12:30:26 -0700

    Colliders

--- Assets/Scripts/DestroyMe.cs
+++ Assets/Scripts/DestroyMe.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class DestroyMe : MonoBehaviour
 {
-    void DestroyObj() {
+    Collider col;
+
+    void Start() {
+        col = GetComponent<Collider>();
+        // Returns the component of type Collider if the GameObject has one attached, null if it doesn't
+        // Don't need to specify exact collider we're usimg
+
+        col.enabled = false;
+        Debug.Log("why");
+        // At start of program, want it to be not disabled (not updated)
+    }
+    public void DestroyObj() {
         Destroy(gameObject);
     } 
+    public void SwitchCollider(int num) {
+        col.enabled = (num == 0) ? false : true;
+        // If num is not 0, then enable Collider col
+    }
 }
